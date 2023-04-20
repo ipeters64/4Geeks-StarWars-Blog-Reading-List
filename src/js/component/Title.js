ef392c820bb20d13efd44c95ef8b2248bd3a4f02
ipeters123 <isaiah.peters64@gmail.com>
2023-04-20 02:01:13 +0000

    comments

--- src/js/component/Title.js
+++ src/js/component/Title.js
@@ -1,7 +1,7 @@
 import React from "react";
 import propTypes from "prop-types";
 
-
+// text is an imported variable defined in propTypes //
 export const Title = ({text}) => {
     return(
         <h1 className="display-5">{text}</h1>
